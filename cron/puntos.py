ba1b67d8afa2445bef75c89a743af619877d1fda
sh4r3m4n <shareman1204@gmail.com>
2012-12-15 15:18:59 +0000

    Los NFU pueden dar 10 puntos por día, pero los novatos solo 3

--- cron/puntos.py
+++ cron/puntos.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-db(db.auth_user.id>0).update(puntos=10)
+#db(db.auth_user.id>0).update(puntos=10)
+
+for user in db(db.auth_user.id>0).select():
+		if db.auth_user.rango(user.id) == T('New Full User'):
+				# Usuario NFU, tiene 10 puntos para dar
+				db(db.auth_user.id==user.id).update(puntos=10)
+		else:
+				# Usuario novato, tiene 3 puntos para dar
+				db(db.auth_user.id==user.id).update(puntos=3)
